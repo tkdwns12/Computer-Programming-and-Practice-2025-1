b29d11b7ba34b442fe44a39fb447a2b730913c48
cm-3c_cd_93_38_B4_95 <cm-3c_cd_93_38_b4_95@AJVDI.INT>
2025-03-21 17:56:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -52,7 +52,13 @@ char input_char();
 
 int main() {
 	/* To Do: 주어진 함수들을 활용해서 문자 하나를 입력받고, 문자 형식과 숫자 형식으로 출력해보세요 */
-
-	
+	char c = input_char();
+	add_to_format('%');
+	add_to_format('c');
+	add_to_format('x');
+	add_to_format('x');
+	add_to_format('%');
+	add_to_format('d');
+	print_formatted(c, c);
 	return 0;
 }
